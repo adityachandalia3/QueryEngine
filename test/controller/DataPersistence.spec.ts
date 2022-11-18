7bc7c6333827f4548957c1f9d1677ca262e61f9f
Keigo <keigol@students.cs.ubc.ca>
2022-11-18 14:19:26 -0800

    comment out code for testing

--- test/controller/DataPersistence.spec.ts
+++ test/controller/DataPersistence.spec.ts
@@ -1,42 +1,42 @@
-import InsightFacade from "../../src/controller/InsightFacade";
+// import InsightFacade from "../../src/controller/InsightFacade";
 
-import chai, {expect} from "chai";
-import chaiAsPromised from "chai-as-promised";
-import {InsightDatasetKind, InsightResult} from "../../src/controller/IInsightFacade";
-import {clearDisk, getContentFromArchives} from "../TestUtil";
-import {Section} from "../../src/controller/Dataset";
-import {loadIds, saveIds, updateIds} from "../../src/controller/FileUtils";
+// import chai, {expect} from "chai";
+// import chaiAsPromised from "chai-as-promised";
+// import {InsightDatasetKind, InsightResult} from "../../src/controller/IInsightFacade";
+// import {clearDisk, getContentFromArchives} from "../TestUtil";
+// import {Section} from "../../src/controller/Dataset";
+// import {loadIds, saveIds, updateIds} from "../../src/controller/FileUtils";
 
-chai.use(chaiAsPromised);
+// chai.use(chaiAsPromised);
 
-describe("DP", function () {
-	let facade: InsightFacade;
+// describe("DP", function () {
+// 	let facade: InsightFacade;
 
-	this.afterEach(async function () {
-		clearDisk();
-	});
+// 	this.afterEach(async function () {
+// 		clearDisk();
+// 	});
 
-	it("save and load", async function () {
-		let arr = ["a", "b"];
-		await saveIds(arr);
-		let brr = await loadIds();
-		expect(brr).to.deep.equal(arr);
-	});
+// 	it("save and load", async function () {
+// 		let arr = ["a", "b"];
+// 		await saveIds(arr);
+// 		let brr = await loadIds();
+// 		expect(brr).to.deep.equal(arr);
+// 	});
 
-	it("load empty", async function () {
-		let arr = await loadIds();
-		expect(arr).to.deep.equal([]);
-	});
+// 	it("load empty", async function () {
+// 		let arr = await loadIds();
+// 		expect(arr).to.deep.equal([]);
+// 	});
 
-	it("updateIds", async function () {
-		let ids = null;
-		ids = await updateIds(ids);
-		expect(ids).to.deep.equal([]);
-	});
+// 	it("updateIds", async function () {
+// 		let ids = null;
+// 		ids = await updateIds(ids);
+// 		expect(ids).to.deep.equal([]);
+// 	});
 
-	it("updateIds", async function () {
-		let ids = ["a"];
-		let ret = await updateIds(ids);
-		expect(ids).to.deep.equal(ret);
-	});
-});
+// 	it("updateIds", async function () {
+// 		let ids = ["a"];
+// 		let ret = await updateIds(ids);
+// 		expect(ids).to.deep.equal(ret);
+// 	});
+// });
