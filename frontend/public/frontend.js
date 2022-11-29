064ce69e92378662c73d73958a57a19bc14b80e2
Keigo <keigol@students.cs.ubc.ca>
2022-11-28 17:43:22 -0800

    add query template function

--- frontend/public/frontend.js
+++ frontend/public/frontend.js
@@ -2,4 +2,42 @@ document.getElementById("click-me-button").addEventListener("click", handleClick
 
 function handleClickMe() {
 	alert("Button Clicked!");
+	console.log(getAveragesQuery("cpsc"));
+}
+
+// This function returns a JSON Query to be used with User Story 1
+function getAveragesQuery(dept) {
+	return {
+		"WHERE": {
+		  "IS": {
+			"sections_dept": dept
+		  }
+		},
+		"OPTIONS": {
+		  "COLUMNS": [
+			"sections_title",
+			"sections_id",
+			"overallAvg"
+		  ],
+		  "ORDER": {
+			"dir": "DOWN",
+			"keys": [
+			  "overallAvg"
+			]
+		  }
+		},
+		"TRANSFORMATIONS": {
+		  "APPLY": [
+			{
+			  "overallAvg": {
+				"AVG": "sections_avg"
+			  }
+			}
+		  ],
+		  "GROUP": [
+			"sections_title",
+			"sections_id"
+		  ]
+		}
+	  };
 }
